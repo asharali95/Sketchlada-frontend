19974742e908c8e8c8833fab2cd9c9818b3e89f0
asharali95 <ashar.alia1999@gmail.com>
2022-01-12 16:41:45 +0500

    frontend realtime notification config

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,28 @@
 import React, { useEffect } from "react";
 import socket from "../../socket";
-
-const Dashboard = () => {
+import { connect } from "react-redux";
+const Dashboard = ({ user }) => {
   useEffect(() => {
     //CDM
     socket.connect();
+    socket.on("notification", (data) => {
+      console.log(data);
+    });
+    return () => {
+      socket.disconnect();
+    };
   }, []);
+
+  useEffect(() => {
+    socket.emit("online", user.userId);
+  }, [user]);
   return (
     <div>
       <h1>Dashboard</h1>
     </div>
   );
 };
-
-export default Dashboard;
+const mapState = (state) => ({
+  user: state.auth,
+});
+export default connect(mapState)(Dashboard);
